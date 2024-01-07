7260f5531dbee5ef0094fe6c963d5641181dce44
Christian <bournecreative@gmail.com>
2024-01-06 22:43:15 -0600

    update eslint and prettier, format files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-explicit-any": "off"
+    "prettier/prettier": ["error", { endOfLine: "off" }],
+    "@typescript-eslint/no-explicit-any": "off",
   },
-}
+};
