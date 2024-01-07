e8dcbce68f09cb7577615df85a273e543d0dfe45
Christian <bournecreative@gmail.com>
2024-01-06 22:39:45 -0600

    rename, reorder interface

---
+++ src/state/reducers/repositoryReducers.ts
@@ -0,0 +1,46 @@
+export enum ActionType {
+  SEARCH_REPOSITORY = "search_packages",
+  SEARCH_REPOSITORY_SUCCESS = "search_packages_success",
+  SEARCH_REPOSITORY_ERROR = "search_packages_error",
+}
+
+// using pure redux, no redux toolkit
+interface RepositoryState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+interface SearchPackages {
+  type: ActionType.SEARCH_REPOSITORY;
+}
+
+interface SearchPackagesSuccess {
+  type: ActionType.SEARCH_REPOSITORY_SUCCESS;
+  payload: string[];
+}
+
+interface SearchPackagesError {
+  type: ActionType.SEARCH_REPOSITORY_ERROR;
+  payload: string;
+}
+
+export type Action =
+  | SearchPackages
+  | SearchPackagesSuccess
+  | SearchPackagesError;
+
+const reducer = (state: RepositoryState, action: Action): RepositoryState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORY:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORY_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORY_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
